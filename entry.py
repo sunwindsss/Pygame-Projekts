8c0b8afce21f0fb222cac1a241f96c677666fdc5
ChristopherSemyonov <mellybang.lav@gmail.com>
2024-01-05 23:41:39 +0200

    Basic functions
    Pievienoju funkcionalitāti

--- entry.py
+++ entry.py
@@ -1,5 +1,23 @@
-# Initial commit
+import pygame
 
-print("Hello, World!")
-print("Šis būs Pygame Projekts mašīnmācīšanās gala darbam!")
-print('Kristofera zars')
+pygame.init()
+screen = pygame.display.set_mode((800, 600)) # flags=pygame.NOFRAME
+icon = pygame.image.load('images/icon.png')
+pygame.display.set_icon(icon)
+
+running = True
+
+while running:
+
+    # screen.fill((252, 186, 3))
+
+    pygame.display.update()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                screen.fill((3, 24, 252))
+                pygame.display.update()
