5bdbb76a02ad60b51a8b5828eb375fc13d7584d0
sunwinds <55029946+sunwindsss@users.noreply.github.com>
2024-01-06 00:31:23 +0200

    Fixed exit error
    pygame.quit() now quits after loop

--- entry.py
+++ entry.py
@@ -1,12 +1,10 @@
 import pygame
 
 pygame.init()
-running = True
 WIDTH = 800
 HEIGHT = 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-
-# Load the icon and set is as the window icon
+background_surface = pygame.Surface((WIDTH, HEIGHT))
 icon = pygame.image.load('images/icon.png')
 pygame.display.set_icon(icon)
 
@@ -14,7 +12,9 @@ pygame.display.set_icon(icon)
 background_image = pygame.image.load('images/grass_bg.png')
 background_image = pygame.transform.scale(background_image, (WIDTH, HEIGHT))
 
-# Initial position of the icon (centered)
+running = True
+
+# Initial position of the icon
 icon_x = (screen.get_width() - 100) // 2
 icon_y = (screen.get_height() - 100) // 2
 
@@ -22,7 +22,6 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-            pygame.quit()
 
     # Get the keys that are currently pressed
     keys = pygame.key.get_pressed()
@@ -44,3 +43,6 @@ while running:
     screen.blit(pygame.transform.scale(icon, (100, 100)), (icon_x, icon_y))
 
     pygame.display.update()
+
+# Quit pygame outside the loop
+pygame.quit()
